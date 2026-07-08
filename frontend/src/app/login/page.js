2c1721c6104a8ae4877f65348900eab4ef18a702
Teo <kingteo692@gmail.com>
2026-07-08 23:05:53 +0200

    Made little changes to the login page

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,20 +1,31 @@
 "use client";
 
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faGoogle } from "@fortawesome/free-solid-svg-icons";
+import { faGoogle, faFacebook } from "@fortawesome/free-brands-svg-icons";
+
+config.autoAddCss = false;
+
 export default function LogIn() {
-  function handleGoogleLogin() {
-    window.location.href =
-      process.env.NEXT_PUBLIC_API_URL + "/api/auth/google/redirect"; // process.env.NEXT_PUBLIC_API_URL must be used everywhere
-  }
-
-  function handleFacebookLogin() {
-    window.location.href =
-      process.env.NEXT_PUBLIC_API_URL + "/api/auth/facebook/redirect";
-  }
-
   return (
-    <main className="p-6 flex flex-col gap-4">
-      <button onClick={handleGoogleLogin}>Login with Google</button>
-      <button onClick={handleFacebookLogin}>Login with Facebook</button>
+    <main className="box-border min-h-screen w-full p-6 flex flex-col items-center justify-center">
+      <div className="flex flex-col gap-4 items-center p-6 rounded-4xl border border-gray-200 shadow-xl">
+        <h1 className="text-xl font-bold">Log in with:</h1>
+        <a
+          href={process.env.NEXT_PUBLIC_API_URL + "/api/auth/google/redirect"}
+          className="bg-blue-500 px-6 py-2 rounded-xl text-white hover:bg-blue-400 transition"
+        >
+          <FontAwesomeIcon icon={faGoogle} /> Login with Google
+        </a>
+        <a
+          href={process.env.NEXT_PUBLIC_API_URL + "/api/auth/google/redirect"}
+          className="bg-blue-500 px-6 py-2 rounded-xl text-white hover:bg-blue-400 transition"
+        >
+          <FontAwesomeIcon icon={faFacebook} /> Login with Facebook
+        </a>
+      </div>
     </main>
   );
 }
