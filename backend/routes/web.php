6fa93a379b400b517dcca1c4553ba7497bf2c28a
Gorazd <gorazd.jovanoski@gmail.com>
2026-07-08 12:22:26 +0200

    branch gorazd

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\SocialLoginController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/auth/{provider}/redirect', [SocialLoginController::class, 'redirect'])->name('social.redirect');
+Route::get('/auth/{provider}/callback', [SocialLoginController::class, 'callback'])->name('social.callback');
+
