a3d7943d6966b186776450e4855e3a9ebce2f42d
Teo <kingteo692@gmail.com>
2026-07-08 15:52:22 +0200

    Mora da pocnuva na api sekoja ruta

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,9 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\SocialLoginController;
 
 
-Route::get('/login', function() {
+Route::get('login', function () {
     return view('login');
 });
-Route::get('/auth/{provider}/redirect', [SocialLoginController::class, 'redirect'])->name('social.redirect');
-Route::get('/auth/{provider}/callback', [SocialLoginController::class, 'callback'])->name('social.callback');
-
+Route::get('api/auth/{provider}/redirect', [SocialLoginController::class, 'redirect'])->name('social.redirect'); // Mora da pocnuva na api sekoja ruta
+Route::get('api/auth/{provider}/callback', [SocialLoginController::class, 'callback'])->name('social.callback');
